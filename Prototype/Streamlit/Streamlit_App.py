5df88715cc1ec1acc8a1981d857d18fb07638323
Silvano <118846332+Silivanili@users.noreply.github.com>
2023-05-20 22:56:33 +0200

    App.py gelöscht, Am Board weitergemacht / umgestellt. Provisorisch db mit den 23k Publikationen genommen.

--- Prototype/Streamlit/Streamlit_App.py
+++ Prototype/Streamlit/Streamlit_App.py
@@ -3,67 +3,104 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from numba import jit
+import psycopg as psycopg
 
+conn = psycopg.connect(host="localhost", dbname="dblp", user="postgres", password="")
 
+cursor = conn.cursor()
 
-st.set_page_config(page_title = "Auswirkungen von Corona auf die Publikationen im Bereich IT", layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title = "Auswirkungen von Corona auf die Publikationen in der DBLP", layout='wide', initial_sidebar_state='expanded')
 
-st.title('Auswirkungen von Corona auf die Publikationen im Bereich IT')
+st.title('Auswirkungen von Corona auf die Publikationen in der DBLP')
 
 with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.sidebar.header("Plot")
+
+info1, info2, info3, info4 = st.columns(4, gap = "medium")
 
 
-dataset = st.container()
+plot1, plot2 = st.columns(2, gap = "large")
 
-with dataset:
-    df = pd.DataFrame(np.random.randint(0,20,size=(10000, 15)), columns=list('ABCDEFGHIJKLMNO'))
+plot3, plot4 = st.columns(2, gap = "large")
 
-st.dataframe(df, use_container_width=True)
+df = pd.DataFrame(np.random.randint(0,20,size=(10000, 15)), columns=list('ABCDEFGHIJKLMNO'))
 
+eins = df.A
 
-#@st.cache_data #When you mark a function with Streamlit’s cache annotation, it tells Streamlit that whenever the function is called that it should check two things: The input parameters you used for the function call. The code inside the function.
-
-
+zwei = df.B
 
 
-class plots:
-    def __init__(self, df):
-        hist_values = np.histogram(df.A)
-        st.bar_chart(hist_values)
-        st.subheader('Test Data')
+with info1:
+    cursor.execute("SELECT title FROM publications")
+    all_titles = cursor.fetchall()
+    st.metric(label = "Publikationen", value = len(all_titles))
 
-add_selectbox = st.sidebar.selectbox("test", "another test")
+with info2:
+    cursor.execute("SELECT name FROM authors")
+    all_authors = cursor.fetchall()
+    st.metric(label="Authoren", value=len(all_authors))
+
+with plot1:
+    st.line_chart(eins)
+    with st.expander("See explanation"):
+        st.write("Das siehts du hier wegen xyz")
+
+with plot2:
+    st.bar_chart(eins)
+
+with plot3:
+    st.area_chart(eins)
+
+with plot4:
+    st.line_chart(zwei)
+
+plot_eins = st.sidebar.header("Plot Nummer 1")
+
+def selectbox(df):
+    sidebar_selectbox = st.sidebar.selectbox("Wähle eine Option", ("Option 1", "Option 2", "Option 3"))
+    return df
+selectbox(df)
+
+plot_zwei = st.sidebar.header("Plot Nummer 2")
 
 def slider(df):
-    hour_to_filter = st.slider('hour', 0, 23, 17)
+    sidebar_slider = st.sidebar.slider('Zeit', 0, 24, 0)
     return df
-
-def iwas(df):
-    test = st.bar_chart(df.B)
-    return df
-
-def nochmehr(df):
-    itest = st.text("Ja Hallo erstmal")
-    return df
-
-nochmehr(df)
-
-st.text("write something, but nothing will happen")
-sel_col = st.container()
-
-def jaja():
-    input_feature = sel_col.text_input("Test","")
 
 slider(df)
 
-plots(df)
 
-iwas(df)
+plot_drei = st.sidebar.header("Plot Nummer 3")
 
-jaja()
+def button(df):
+    sidebar_button = st.sidebar.button("Des isch a Knopf")
+    return df
+button(df)
+
+def checkbox(df):
+    sidebar_checkbox = st.sidebar.checkbox("Zeig etwas an")
+    return df.A
+
+checkbox(df)
+
+
+plot_vier = st.sidebar.header("Plot Nummer 4")
+
+def date_input(df):
+    sidebar_date_input = st.sidebar.date_input("test")
+    return df
+
+date_input(df)
+
+
+plot_vier = st.sidebar.header("Plot Nummer 5")
+
+
+
+
+
+
 
 #Um das Dashobard zu starten, folgende Zeile in die Anaconda Powershell, am Ort des CodingProjekt, eingeben:
 #streamlit run Streamlit_App.py
