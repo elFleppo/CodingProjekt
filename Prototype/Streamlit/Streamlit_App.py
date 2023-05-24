6fef76991d3e84a3e12724f8b71fa93a1c89e47c
Silvano <118846332+Silivanili@users.noreply.github.com>
2023-05-24 20:31:05 +0200

    Code "verschönert" und besser beschrieben, mit caching angefangen, aber unsicher ob gut so, werde mich informieren. Im CSS noch die Umrandung für die Metrik geaddet.

--- Prototype/Streamlit/Streamlit_App.py
+++ Prototype/Streamlit/Streamlit_App.py
@@ -19,10 +19,29 @@ stop_words = set(stopwords.words('english'))
 #conn = psycopg.connect(host="localhost", dbname="dblp", user="postgres", password="")
 
 
-st.set_page_config(page_title = "Auswirkungen von Corona auf die Publikationen in der DBLP", layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title = "Auswirkungen von Corona auf die Publikationen in der DBLP", layout='wide', initial_sidebar_state='expanded')   #Muss erste Zeile von st. sein
+
+@st.cache_resource(show_spinner="Lade die Daten aus der Datenbank...")          #https://docs.streamlit.io/library/advanced-features/caching
+def db_connection():
+    return st.experimental_connection(name = "dblpppppp", type="sql", max_entries=None, ttl=None, autocommit=True)
+
+conn = db_connection()
+@st.cache_data              #Cache diese Querry. Geht nur als Funktion.
+def table_publications():
+    return conn.query("select * from publications")
+@st.cache_data
+def all_publications():
+    return conn.query("select title from publications")
+@st.cache_data
+def all_authors():
+    return conn.query("select name from authors")
 
 
-conn = st.experimental_connection(name = "dblp", type="sql", max_entries=None, ttl=None, autocommit=True)
+table_publications = table_publications()               #Bin mir unsicher ob das so gut ist, werde mich informieren. 
+
+all_publications = all_publications()
+
+all_authors = all_authors()
 
 
 st.title("Auswirkungen von Corona auf die Publikationen in der DBLP")
@@ -40,29 +59,54 @@ plot1, plot2 = st.columns(2, gap = "large")     #2x2 Matrix an Plots
 plot3, plot4 = st.columns(2, gap = "large")
 
 
-df = conn.query("select * from publications")
+def readDataToList(all_publications):
+    most_common_num = 10
+    keywords = []
+    word_list = all_publications.split()
+    for word in word_list:
+        if "." in word:
+            word = word.replace(".", "")
+        if "-" in word:
+            word = word.replace("-", "")
+        if word.lower() not in stop_words:
+            keywords.append(word.lower())
+    new_list = list(filter(lambda x: x != "", keywords))            #"" ist ein eigener Char in Keywords, darum wird das auch ersetzt.
+    # with open("keywords.txt", "w", encoding="utf-8") as file:
+    # file.write(str(new_list))
+    counter = collections.Counter(new_list)                         #Gibt ein Dict aus. Das Wort ist der Key, und die Häufigkeit das Value.
+    most_common = counter.most_common(most_common_num)
+    return most_common                                              #Sieht so aus [('none', 528876), ('home', 12867), ('page', 12866), ('data', 2192)]
+
+
+
 
 with info1:
-    all_publications = conn.query("select title from publications")
     st.metric(label = "Publikationen", value = len(all_publications))
 
 with info2:
-    all_authors = conn.query("select name from authors")
     st.metric(label="Authoren", value=len(all_authors))
 
 with plot1:
-    st.line_chart(df.pubyear)
+    #ERSTER PLOT HIER
     with st.expander("See explanation"):
         st.write("Das siehts du hier wegen xyz")
+        pass
 
 with plot2:
-    st.bar_chart(df.pubyear)
+    #ZWEITER PLOT HIER
+    pass
 
 with plot3:
-    st.area_chart(df.pubyear)
+    #DRITTER PLOT HIER
+    pass
 
 with plot4:
-    st.line_chart(df.pubyear)
+    #VIERTER PLOTT HIER
+    pass
+
+
+##################################################### S I D E B A R ####################################################
+
 
 tab1, tab2 = st.sidebar.tabs(["Eingabe", "Detail Bar"])
 
@@ -75,16 +119,18 @@ with tab1:
         return "test"
     selectbox()
 
+
     plot_zwei = st.header("Plot Nummer 2")
+
 
     def slider():
         sidebar_slider = st.slider('Zeit', 0, 24, 0)
         return "test"
-
     slider()
 
 
     plot_drei = st.header("Plot Nummer 3")
+
 
     def button():
         sidebar_button = st.button("Hit me")
@@ -94,55 +140,39 @@ with tab1:
     def checkbox():
         sidebar_checkbox = st.checkbox("Zeig etwas an")
         return "test"
-
     checkbox()
 
 
     plot_vier = st.header("Plot Nummer 4")
 
+
     def date_input():
         sidebar_date_input = st.date_input("test")
         return "test"
-
     date_input()
 
 
-    plot_vier = st.header("Plot Nummer 5")
+    plot_fünf = st.header("Plot Nummer 5")
 
 
 with tab2:
     plot_detail = st.header("Detailplot Nummer 1")
-    counter = df.pubyear.value_counts()
-    st.line_chart(counter)
+
+
+    publications_per_year = table_publications.pubyear.where(table_publications.pubyear > "1993")
+    st.bar_chart(publications_per_year)
+
 
     plot_detail2 = st.header("Detailplot Nummer 2")
-    df = df.drop(["mdate", "publtype", "key", "pubyear"], axis = 1)
-    all_publications= df.to_string(decimal = ";")
 
-
-    def readDataToList(all_publications):
-        most_common_num = 100
-        keywords = []
-        word_list = all_publications.split()
-        for word in word_list:
-            if "." in word:
-                word = word.replace(".", "")
-            if "-" in word:
-                word = word.replace("-", "")
-            if word.lower() not in stop_words:
-                keywords.append(word.lower())
-        new_list = list(filter(lambda x: x != "", keywords))
-        #with open("keywordstest.txt", "w", encoding="utf-8") as file:
-            #file.write(str(new_list))
-        counter = collections.Counter(new_list)
-        most_common = counter.most_common(most_common_num)
-        return most_common
-
+    just_publications = table_publications.drop(["mdate", "publtype", "key", "pubyear"], axis = 1)
+    #print(just_publications.describe())
+    all_publications = just_publications.to_string(decimal = ";", index = False)           #Series_to_string geht leider nicht, darum so
 
     histvalues = readDataToList(all_publications)
 
     def histogram(histvalues):
-        histdf = pd.DataFrame(data = histvalues, columns=["word", "counting"])
+        histdf = pd.DataFrame(data = histvalues, columns=["word", "counting"])          #Wandle Liste in df zurück
         st.bar_chart(data = histdf.counting)
         st.write(histdf)
 
